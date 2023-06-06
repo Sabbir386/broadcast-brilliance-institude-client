9bceca90064c31de2b321222aadb0844be27670d
Sabbir386 <sabbir386.sa@gmail.com>
2023-06-06 22:09:39 +0600

    added max width responsive setup all over project

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+
+    <div className=' px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
