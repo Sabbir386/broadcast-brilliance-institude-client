7c8971d7ebfde3b6f23594c8a8702596a876f29b
Sabbir386 <sabbir386.sa@gmail.com>
2023-06-09 02:24:38 +0600

    added user admin role

--- src/Componenets/AdminDashboard/ManageUsers.jsx
+++ src/Componenets/AdminDashboard/ManageUsers.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { FaTrashAlt, FaUser } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const ManageUsers = () => {
     const { isLoading, refetch, isError, data: usersData = [], error } = useQuery({
@@ -13,33 +14,53 @@ const ManageUsers = () => {
         }
     })
     const handleDelete = (id) => {
-        Swal.fire({
-            title: 'Are you sure,want to delete this User?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                fetch(`http://localhost:5000/bookingClass/${id}`, {
-                    method: 'DELETE'
-                })
-                    .then(res => res.json())
-                    .then(result => {
-                        if (result.deletedCount) {
-                            refetch();
-                            Swal.fire(
-                                'Deleted!',
-                                'Your select class has been deleted.',
-                                'success'
-                            )
-                        }
-                    })
+        // Swal.fire({
+        //     title: 'Are you sure,want to delete this User?',
+        //     text: "You won't be able to revert this!",
+        //     icon: 'warning',
+        //     showCancelButton: true,
+        //     confirmButtonColor: '#3085d6',
+        //     cancelButtonColor: '#d33',
+        //     confirmButtonText: 'Yes, delete it!'
+        // }).then((result) => {
+        //     if (result.isConfirmed) {
+        //         fetch(`http://localhost:5000/bookingClass/${id}`, {
+        //             method: 'DELETE'
+        //         })
+        //             .then(res => res.json())
+        //             .then(result => {
+        //                 if (result.deletedCount) {
+        //                     refetch();
+        //                     Swal.fire(
+        //                         'Deleted!',
+        //                         'Your select class has been deleted.',
+        //                         'success'
+        //                     )
+        //                 }
+        //             })
 
-            }
+        //     }
+        // })
+    }
+
+    const handleAdmin = (id) => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: 'PATCH'
         })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: ' Admin Now',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+
+            })
     }
     return (
         <div className='w-full'>
@@ -53,27 +74,32 @@ const ManageUsers = () => {
                             <th>#</th>
                             <th>User-Name</th>
                             <th>User-Email</th>
-                            <th>Role</th>
-                            <th>Activity</th>
+                            <th>Admin-Role</th>
+                            <th>Instructor-Role</th>
                         </tr>
                     </thead>
                     <tbody className=''>
                         {
-                            usersData.map((dataOfUSer, index) => <tr
+                            usersData.map((user, index) => <tr
+                                key={user._id}
 
                             >
                                 <th>{index + 1}</th>
-                                <td>{dataOfUSer.name}</td>
-                                <td>{dataOfUSer.email}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
                                 <td>{
-                                    usersData.role === 'admin' ? 'admin' :
+                                    user.role === 'admin' ? 'admin' :
 
-                                        <div onClick={() => handleDelete(usersData._id)} className='mx-auto text-blue-500 h-4 w-6 cursor-pointer'><FaUser></FaUser></div>
+                                        <div onClick={() => handleAdmin(user._id)} className='mx-auto text-blue-500 h-4 w-6 cursor-pointer'><FaUser></FaUser></div>
 
                                 }
                                 </td>
-                                <td className='flex'>
-                                    <FaTrashAlt onClick={() => handleDelete(usersData._id)} className='mx-auto text-red-500 h-4 w-6 cursor-pointer'>Delete</FaTrashAlt>
+                                <td className=''>
+                                    {
+                                        user.role === 'instructor' ? 'instructor' :
+
+                                            <div onClick={() => handleInstructor(user._id)} className='mx-auto text-blue-500 h-4 w-6 cursor-pointer'><FaUser></FaUser></div>
+                                    }
 
                                 </td>
                             </tr>)
